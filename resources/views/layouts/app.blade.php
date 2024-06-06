e3e5c6fd9890b73192ef841fa70dda1723a42efa
unknown <sabdulsalam@ubtconsults.se>
2024-06-06 15:51:47 +0100

    fix: updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -478,11 +478,11 @@ $routeWithoutSearch = [
 </head>
 
 <body class="m-0 font-sans antialiased font-normal  text-base leading-default bg-[skyblue]/75 text-slate-500 overflow-hidden">
-<div id="mainLoader" class="bg-sky-500/70 inset-0 fixed z-[100001] flex flex-col justify-center items-center   overflow-y-auto h-full w-ful" >
+<div id="mainLoader" class="bg-sky-500 inset-0 fixed z-[100001] w-full flex flex-col justify-center items-center   overflow-y-auto h-full" >
             <span class="loader"></span>
             <span class="text-2xl">loading</span>
 </div>
-<canvas id="canvas" class="absolute z-[-1] w-full block"></canvas>
+    <canvas id="canvas" class="absolute z-[-1] w-full block"></canvas>
     <!-- <div class="absolute bg-border-radius  z-[-2] w-full bg-[skyblue]/75 dark:hidden" style="height: 300px;"></div> -->
     <main class="lg:ml-auto h-full max-h-screen transition-all duration-200 ease-in-out rounded-xl ps  xl:px-5 px-2  pl-2 py-5">
 
@@ -738,11 +738,13 @@ $routeWithoutSearch = [
                 document.getElementById('app').classList.remove('hidden')
             }
             }).mount('#app')
+
+            setTimeout(()=>{
+                document.getElementById("mainLoader").style.display = 'none';
+
+            },1000)
         }
 
-        $(document).ready(function(){
-            $("#mainLoader").hide();
-        });
     </script>
 
     <style>
