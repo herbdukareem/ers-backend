369034b1faab33169768af97ba043b51585fc4b9
unknown <sabdulsalam@ubtconsults.se>
2024-06-30 15:22:33 +0100

    fix:updates

--- resources/views/v1/executive_dashboard.blade.php
+++ resources/views/v1/executive_dashboard.blade.php
@@ -23,7 +23,7 @@ $facilities = Facility::all();
         <i @click="toggleFullscreen('appRoot2')" :class="isFullScreen? 'fa-solid' : ' fa-regular'" class=" fa-window-maximize place-self-end cursor-pointer hover:text-[#1BFFFF]" :title="isFullScreen?'Minimize':'Maximize'"></i>
     </div>
     <div class="grid md:grid-cols-3 grid-cols-1 w-full gap-5">
-        <div class="chart-container px-3 py-2 grid grid-cols-1  place-content-center gap-3">
+        <div class="chart-container md:col-span-3 px-3 py-2 grid grid-cols-1  place-content-center gap-3">
             <div class="grid sm:grid-cols-2 grid-cols-1">
                 <span class="place-self-center">Total Enrollee:</span>
                 <h4 class=" font-bold text-xl place-self-center">@{{encountersAnalytics?.totalEnrolleesAll}}</h4>
